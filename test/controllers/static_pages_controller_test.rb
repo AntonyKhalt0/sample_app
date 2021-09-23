e843460908e46ab878a1268ca8db289307118393
AntonyKhalt0 <toscha.xalturin@gmail.com>
2021-09-23 17:04:06 +0300

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get :home
+    get :root
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App" 
   end
