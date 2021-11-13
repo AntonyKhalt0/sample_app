d14745fbb750a0c0bba87e3264aa6d80ac68c9a6
AntonyKhalt0 <toscha.xalturin@gmail.com>
2021-11-13 12:09:49 +0300

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController #Контроллер пользователя
 
-  def show
+  def show # Метод вывода пользователя
     @user = User.find(params[:id])
   end
 
-  def new
+  def new # Метод создания пользователя
     @user = User.new
   end
 
-  def create
+  def create # Метод создания и сохранения пользователя 
     @user = User.new(user_params)
-    if @user.save
+    if @user.save # Вход в лк, если пользователь сохранен
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-    else
+    else # Если пользватель не сохранен, создается новый пользователь
       render 'new'
     end
   end
 
   private
 
-    def user_params
+    def user_params # Метод передачи данных пользователя
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
